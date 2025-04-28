589f604b6cae2c2e1c27f7dd488d1c07ce5b8735
DEV-COM <kwp@tlen.pl>
2025-04-28 12:31:33 +0200

    Refactor skip conditions in conftest.py for improved clarity and consistency

--- dzien2/conftest.py
+++ dzien2/conftest.py
@@ -5,6 +5,7 @@ import os
 # Definicja warunków skip
 only_linux = sys.platform != "linux"
 is_windows = sys.platform == 'win32'
+is_linux = sys.platform == 'linux'
 only_python_11 = sys.version_info[:2] != (3,11)
 ci_env = os.getenv("CI") == 'true'
 
@@ -16,9 +17,9 @@ def sample_numbers():
 # skipowanie globalne
 
 def pytest_collection_modifyitems(config,items):
-    skip_windows = pytest.mark.skip(reason="pomijam testy na win")
-    if is_windows:
+    skip_linux = pytest.mark.skip(reason="pomijam testy na linuxa")
+    if is_linux:
         for item in items:
-            item.add_marker(skip_windows)
+            item.add_marker(skip_linux)
 
 
