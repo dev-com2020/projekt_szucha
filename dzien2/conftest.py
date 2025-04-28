429121089be60ac16d832a54060ac16a24764d62
DEV-COM <kwp@tlen.pl>
2025-04-28 12:29:10 +0200

    Refactor test skipping conditions and update test_smart_skipping to use centralized definitions

--- dzien2/conftest.py
+++ dzien2/conftest.py
@@ -1,5 +1,24 @@
 import pytest
+import sys
+import os
 
+# Definicja warunków skip
+only_linux = sys.platform != "linux"
+is_windows = sys.platform == 'win32'
+only_python_11 = sys.version_info[:2] != (3,11)
+ci_env = os.getenv("CI") == 'true'
+
+# globalne fixtury
 @pytest.fixture
 def sample_numbers():
     return 6,3
+
+# skipowanie globalne
+
+def pytest_collection_modifyitems(config,items):
+    skip_windows = pytest.mark.skip(reason="pomijam testy na win")
+    if is_windows:
+        for item in items:
+            item.add_marker(skip_windows)
+
+
